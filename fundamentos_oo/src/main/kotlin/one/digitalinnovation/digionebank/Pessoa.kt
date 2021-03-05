6dc5478b3291648972fab1ccced0faa68094724b
diegohfcelestino <diegohfcelestino@gmail.com>
2021-03-05 20:30:55 -0300

    teste de commit

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,7 +1,14 @@
 package one.digitalinnovation.digionebank
 
 abstract class Pessoa(
-    val nome: String,
-    val cpf: String
+    val nome: String = "Diego",
+    val cpf: String = "123.123.123-12"
 )
+
+fun main() {
+    val diego =  Pessoa()
+
+    println(diego.nome)
+    println(diego.cpf)
+}
 
