eca7229092c63acf0c1f83e77a2873910b897820
diegohfcelestino <diegohfcelestino@gmail.com>
2021-03-05 22:06:53 -0300

    voltando ao codigo anterior

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,17 +1,7 @@
 package one.digitalinnovation.digionebank
 
-abstract class Pessoa {
-    var nome: String = "Diego"
-
-    var cpf: String = "123.123.123-12"
-    private set
-}
-
-fun main () {
-    val diego = Pessoa()
-
-    println(diego)
-    println(diego.nome)
-    println(diego.cpf)
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
 
