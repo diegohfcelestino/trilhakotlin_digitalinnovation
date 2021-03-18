eb0b7960008352fc7f30df62ee93b98dfb109396
diegohfcelestino <diegohfcelestino@gmail.com>
2021-03-17 22:33:42 -0300

    Criado Arrays max, min, average, filter, count, find e any

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -1,29 +1,37 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+   val salarios = doubleArrayOf(1200.0, 2590.0, 5900.0)
 
-    for (salario in salarios) {
-        println(salario)
-    }
+   for (salario in salarios) {
+       println(salario)
+   }
+    println("-------")
 
-    println("--------------------")
     println("Maior salario: ${salarios.maxOrNull()}")
     println("Menor salario: ${salarios.minOrNull()}")
     println("Media salarial: ${salarios.average()}")
 
+    println("---------")
+
+    //salarios maior que 2.550,00
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
-    println("--------------------")
     salariosMaiorQue2500.forEach { println(it) }
 
-    println("--------------------")
-    println(salarios.count { it in 2000.0..5000.0 })
+    println("---------")
 
-    println("--------------------")
-    println(salarios.find { it == 2250.0 })
+    //imprimir a quantidade de elementos dentro de um renge de 2500 até 5500
+    println(salarios.count { it in 2500.0..6500.0 })
+
+    println("---------")
+
+    //buscar algum valor especifico
+    println(salarios.find { it == 1200.0 })
     println(salarios.find { it == 500.0 })
 
-    println("--------------------")
-    println(salarios.any { it == 1000.0 })
+    println("---------")
+
+    //buscar qualquer que seja o valor especico
+    println(salarios.any { it == 2590.0 })
     println(salarios.any { it == 500.0 })
 }
