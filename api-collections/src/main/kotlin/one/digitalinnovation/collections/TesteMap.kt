b3265b6274370a3a6d6c8d6d36060c9319e3e421
diegohfcelestino <diegohfcelestino@gmail.com>
2021-03-17 23:29:11 -0300

    Desenvolvendo com mapOf

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
@@ -1,14 +1,18 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val pair: Pair<String, Double> = Pair("Joao", 1000.0)
+    //primeira forma de fazer um map
+    val pair: Pair<String, Double> = Pair("Lara", 2000.0)
     val map1= mapOf(pair)
 
     map1.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
 
+    println("----------")
+
+    //segunda forma de fazer um map
     val map2 = mapOf(
-        "Pedro" to 2500.0,
-        "Maria" to 3000.0
+        "Eric" to 2800.0,
+        "Aila" to 3900.0
     )
 
     map2.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
