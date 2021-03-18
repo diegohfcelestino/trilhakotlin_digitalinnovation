1a83c13109570e32c6b428c3d728388aceb5c6ab
diegohfcelestino <diegohfcelestino@gmail.com>
2021-03-17 21:31:46 -0300

    Foi estudado Int Array e atuazamos a versão do mavem

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -2,27 +2,32 @@ package one.digitalinnovation.collections
 
 fun main() {
     val values = IntArray(5)
+
     values[0] = 1
     values[1] = 7
     values[2] = 6
     values[3] = 3
     values[4] = 2
 
+    //aqui declaramos explicitamente
     for (valor in values) {
         println(valor)
     }
+    println("--------------")
 
-    println("-----------------------------")
+    //aqui usamos uma expressão lambida
     values.forEach { valor ->
         println(valor)
     }
+    println("--------------")
 
-    println("-----------------------------")
+    //aqui estamos iterando sobre os indeces de cada array
     for (index in values.indices) {
         println(values[index])
     }
+    println("--------------")
 
-    println("-----------------------------")
+    //aqui colocamos para ordenar do maior para o menor (sort) antes de printar
     values.sort()
     for (valor in values) {
         println(valor)
