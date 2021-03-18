6c516b79833092621836e2c23a9e603cd6ec5f52
diegohfcelestino <diegohfcelestino@gmail.com>
2021-03-17 22:13:00 -0300

    Criado Double Array

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteDoubleArray.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteDoubleArray.kt
@@ -2,6 +2,35 @@ package one.digitalinnovation.collections
 
 fun main() {
     val salarios = DoubleArray(3)
+    salarios[0] = 1500.0
+    salarios[1] = 5000.0
+    salarios[2] = 900.00
+
+    //aqui é a iteração lambida
+    salarios.forEach {
+        println(it)
+    }
+    println("-------------------------")
+
+    //aqui estamos iterando acrescentando 10% de salario com expressão lambida
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach {
+        println(it)
+    }
+    println("-------------------------")
+
+    //aqui inicializamos com valor e já fizemos a ordenação com o sort
+    val salarios2 = doubleArrayOf(1950.0, 6250.0, 1800.0)
+    salarios2.sort()
+    salarios2.forEach { println("Os salarios2 são: " + it) }
+
+
+}
+
+/*fun main() {
+    val salarios = DoubleArray(3)
     salarios[0] = 1000.0
     salarios[1] = 3000.0
     salarios[2] = 500.0
@@ -19,3 +48,5 @@ fun main() {
     salarios2.sort()
     salarios2.forEach { println(it) }
 }
+
+ */
